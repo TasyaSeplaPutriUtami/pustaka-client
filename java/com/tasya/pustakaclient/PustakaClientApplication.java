4bc028f4608bf89e63fe9b5795752fa5daf84b85
TasyaSeplaPutriUtami <113502811+TasyaSeplaPutriUtami@users.noreply.github.com>
2023-01-04 22:17:01 +0700

    Add files via upload

---
+++ java/com/tasya/pustakaclient/PustakaClientApplication.java
@@ -0,0 +1,13 @@
+package com.tasya.pustakaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PustakaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PustakaClientApplication.class, args);
+	}
+
+}
